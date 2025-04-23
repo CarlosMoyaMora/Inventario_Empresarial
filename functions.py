eceb1726bf222aadacbe33f9257c089daec178cf
Carlos Moya Mora <moyabmx1996@gmail.com>
2025-04-22 18:02:14 -0600

    Update functions.py

--- functions.py
+++ functions.py
@@ -12,5 +12,19 @@ def consulta():
     pass
 
 
-def entrada_inv():
-    
+def entrada_inv(nuevo_ingreso):
+    while True:    
+        
+        try:
+            nombre_art = input('Ingrese el nombre del Articulo: ')
+            cantidad = int(input('Digite la cantidad que deseea ingresar al Sistema: '))
+            precio = float(input('Ingrese el Valor del articulo: '))
+            fecha_ing = input('Ingrese la fecha del ingreso: ')
+        except ValueError:
+                print('El valor no es correcto, por favor intentelo nuevamente!')
+                continue    
+            
+        ingreso = {'nombre': nombre_art, 'cantidad' : cantidad, 'precio' : precio, 'fecha' : fecha_ing} 
+        
+        nuevo_ingreso.append(ingreso)
+       
